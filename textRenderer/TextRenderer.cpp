4b44e3497f03606b3327151b150e0843def30601
Carlos Valera <2501087+cookyt@users.noreply.github.com>
2023-11-12 10:32:40 -0500

    Add scaling factor to text rendering.
    Also refactors to define an options struct to reduce the number of
    params running around.

--- textRenderer/TextRenderer.cpp
+++ textRenderer/TextRenderer.cpp
@@ -2,48 +2,61 @@
 
 namespace pico_ssd1306 {
 
-    void drawText(pico_ssd1306::SSD1306 *ssd1306, const unsigned char *font, const char *text, uint8_t anchor_x,
-                  uint8_t anchor_y, WriteMode mode, Rotation rotation) {
-        if(!ssd1306 || !font || !text) return;
-
-        uint8_t font_width = font[0];
-
+    void drawText(pico_ssd1306::SSD1306 *ssd1306, const char *text, uint8_t anchor_x,
+                  uint8_t anchor_y, const DrawTextOptions& opts) {
         uint16_t n = 0;
         while (text[n] != '\0') {
-            switch (rotation) {
+            uint8_t x = 0xFF, y = 0xFF;
+            switch (opts.rotation) {
                 case Rotation::deg0:
-                    drawChar(ssd1306, font, text[n], anchor_x + (n * font_width), anchor_y, mode, rotation);
+                    x = anchor_x + (n * opts.font_width());
+                    y = anchor_y;
                     break;
                 case Rotation::deg90:
-                    drawChar(ssd1306, font, text[n], anchor_x, anchor_y + (n * font_width), mode, rotation);
+                    x = anchor_x;
+                    y = anchor_y + (n * opts.font_width());
                     break;
             }
+            if (x > ssd1306->getWidth() || y > ssd1306->getHeight()) {
+                break;
+            }
+            drawChar(ssd1306, text[n], x, y, opts);
 
             n++;
         }
     }
 
-    void drawChar(pico_ssd1306::SSD1306 *ssd1306, const unsigned char *font, char c, uint8_t anchor_x, uint8_t anchor_y,
-                  WriteMode mode, Rotation rotation) {
-        if(!ssd1306 || !font || c < 32) return;
+    static void setScaledPixel(pico_ssd1306::SSD1306 *ssd1306, uint8_t anchor_x,
+                               uint8_t anchor_y, const DrawTextOptions& opts) {
+        for (int x = anchor_x; x < anchor_x + opts.scale_x; ++x) {
+            for (int y = anchor_y; y < anchor_y + opts.scale_y; ++y) {
+                ssd1306->setPixel(x, y, opts.mode);
+            }
+        }
+    }
 
-        uint8_t font_width = font[0];
-        uint8_t font_height = font[1];
+    void drawChar(pico_ssd1306::SSD1306 *ssd1306, char c, uint8_t anchor_x,
+                  uint8_t anchor_y, const DrawTextOptions& opts) {
+        if (c < 32) return;
 
-        uint16_t seek = (c - 32) * (font_width * font_height) / 8 + 2;
+        uint16_t font_bit_size = opts.font[0] * opts.font[1];
+        uint16_t seek = (c - 32) * font_bit_size / 8 + 2;
 
         uint8_t b_seek = 0;
 
-        for (uint8_t x = 0; x < font_width; x++) {
-            for (uint8_t y = 0; y < font_height; y++) {
-                if (font[seek] >> b_seek & 0b00000001) {
-                    switch (rotation) {
+        for (uint8_t x = 0; x < opts.font_width(); x += opts.scale_x) {
+            for (uint8_t y = 0; y < opts.font_height(); y += opts.scale_y) {
+                if (opts.font[seek] >> b_seek & 0b00000001) {
+                    switch (opts.rotation) {
                         case Rotation::deg0:
-                            ssd1306->setPixel(x + anchor_x, y + anchor_y, mode);
+                            setScaledPixel(ssd1306, x + anchor_x, y + anchor_y,
+                                           opts);
                             break;
                         case Rotation::deg90:
-                            ssd1306->setPixel(-y + anchor_x + font_height, x + anchor_y, mode);
-                            break;
+                          setScaledPixel(ssd1306,
+                                         -y + anchor_x + opts.font_height(),
+                                         x + anchor_y, opts);
+                          break;
                     }
                 }
                 b_seek++;
--- textRenderer/TextRenderer.h
+++ textRenderer/TextRenderer.h
@@ -18,6 +18,23 @@ namespace pico_ssd1306{
         deg90,
     };
 
+    // TODO: field descriptions
+    struct DrawTextOptions {
+        const unsigned char *font;
+        WriteMode mode = WriteMode::ADD;
+        Rotation rotation = Rotation::deg0;
+        uint8_t scale_x = 1;
+        uint8_t scale_y = 1;
+
+        uint8_t font_width() const { return font[0] * scale_x; }
+        uint8_t font_height() const { return font[1] * scale_y; }
+    };
+
+    // TODO: param descriptions
+    void drawChar(pico_ssd1306::SSD1306 *ssd1306,
+                  char c, uint8_t anchor_x, uint8_t anchor_y,
+                  const DrawTextOptions& opts);
+
     /// \brief Draws a single glyph on the screen
     /// \param ssd1306 - pointer to a SSD1306 object aka initialised display
     /// \param font - pointer to a font data array
@@ -25,7 +42,18 @@ namespace pico_ssd1306{
     /// \param anchor_x, anchor_y - coordinates setting where to put the glyph
     /// \param mode - mode describes setting behavior. See WriteMode doc for more information
     /// \param rotation - either rotates the char by 90 deg or leaves it unrotated
-    void drawChar(pico_ssd1306::SSD1306 *ssd1306, const unsigned char * font, char c, uint8_t anchor_x, uint8_t anchor_y, WriteMode mode = WriteMode::ADD, Rotation rotation = Rotation::deg0);
+    inline void drawChar(pico_ssd1306::SSD1306 *ssd1306, const unsigned char *font,
+                  char c, uint8_t anchor_x, uint8_t anchor_y,
+                  WriteMode mode = WriteMode::ADD,
+                  Rotation rotation = Rotation::deg0) {
+      drawChar(ssd1306, c, anchor_x, anchor_y,
+               {.font = font, .mode = mode, .rotation = rotation});
+    }
+
+    // TODO: param descriptions
+    void drawText(pico_ssd1306::SSD1306 *ssd1306, const char *text,
+                  uint8_t anchor_x, uint8_t anchor_y,
+                  const DrawTextOptions &opts);
 
     /// \brief Draws text on screen
     /// \param ssd1306 - pointer to a SSD1306 object aka initialised display
@@ -34,7 +62,14 @@ namespace pico_ssd1306{
     /// \param anchor_x, anchor_y - coordinates setting where to put the text
     /// \param mode - mode describes setting behavior. See WriteMode doc for more information
     /// \param rotation - either rotates the text by 90 deg or leaves it unrotated
-    void drawText(pico_ssd1306::SSD1306 *ssd1306, const unsigned char * font, const char * text, uint8_t anchor_x, uint8_t anchor_y, WriteMode mode = WriteMode::ADD, Rotation rotation = Rotation::deg0);
+    inline void drawText(pico_ssd1306::SSD1306 *ssd1306,
+                         const unsigned char *font, const char *text,
+                         uint8_t anchor_x, uint8_t anchor_y,
+                         WriteMode mode = WriteMode::ADD,
+                         Rotation rotation = Rotation::deg0) {
+      drawText(ssd1306, text, anchor_x, anchor_y,
+               {.font = font, .mode = mode, .rotation = rotation});
+    }
 }
 
 #endif //SSD1306_TEXTRENDERER_H
